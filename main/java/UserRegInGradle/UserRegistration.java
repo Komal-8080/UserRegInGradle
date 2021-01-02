66567d1adde329877f00da342bbbccf4318fbcfb
Komal-8080 <shindeg163@gmail.com>
2021-01-03 00:51:14 +0530

    Modified: Methods modified to return as Happy or Sad while performing Junit Test

--- main/java/UserRegInGradle/UserRegistration.java
+++ main/java/UserRegInGradle/UserRegistration.java
@@ -5,44 +5,64 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public static boolean FirstName() {		
+	public static String FirstName() {		
 		Pattern firstName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");  
 		Matcher match = firstName.matcher("Komal");  
 		boolean patternValidity = match.matches();
-		System.out.println(patternValidity);
-		return patternValidity;		
+			if (patternValidity == true) { 
+				return "HAPPY";
+			}
+			else {
+				return "SAD";
+			}
 	}
 	
-	public static boolean LastName() {		
+	public static String LastName() {		
 		Pattern lastName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");  
 		Matcher match = lastName.matcher("Shinde");  
 		boolean patternValidity = match.matches();
-		System.out.println(patternValidity);
-		return patternValidity;	
+			if (patternValidity == true) { 
+				return "HAPPY";
+			}
+			else {
+				return "SAD";
+			}
 	}
 	
-	public static boolean Email() {
+	public static String Email() {
 		Pattern email = Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1,}@[a-zA-Z]{2}\\.[a-zA-Z]{2}\\.[a-zA-Z]{1,}$");  
 		Matcher match = email.matcher("abc.xyz@bl.co.in");  
 		boolean patternValidity = match.matches();
-		System.out.println(patternValidity);
-		return patternValidity;
+			if (patternValidity == true) { 
+				return "HAPPY";
+			}
+			else {
+				return "SAD";
+			}
 	}
 	
-	public static boolean MobileNumber() {
+	public static String MobileNumber() {
 		Pattern mobileNumber = Pattern.compile("^[1-9]{2}\\s{1}[0-9]{1}[0-9]{9}$");  
 		Matcher match = mobileNumber.matcher("91 8562416354");  
 		boolean patternValidity = match.matches();
-		System.out.println(patternValidity);
-		return patternValidity;		
+			if (patternValidity == true) { 
+				return "HAPPY";
+			}
+			else {
+				return "SAD";
+			}	
 	}
 	
-	public static boolean Password() {
+	public static String Password() {
 		Pattern mobileNumber = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}");  
 		Matcher match = mobileNumber.matcher("Komal@80");  
 		boolean patternValidity = match.matches();
-		System.out.println(patternValidity);
-		return patternValidity;
+			if (patternValidity == true) { 
+				return "HAPPY";
+			}
+			else {
+				return "SAD";
+			}
 	}
 	
 	public static boolean SampleEmailValidation() {
--- test/java/UserRegInGradle/UserRegistrationTest.java
+++ test/java/UserRegInGradle/UserRegistrationTest.java
@@ -7,20 +7,23 @@ public class UserRegistrationTest {
 	
 	public static void FirstName() {
 		
+		String Happy = null;
+		
 		UserRegistration ur = new UserRegistration();
-		boolean generated_output1 = ur.FirstName();
-		boolean generated_output2 = ur.LastName();
-		boolean generated_output3 = ur.Email();
-		boolean generated_output4 = ur.MobileNumber();
-		boolean generated_output5 = ur.Password();
+		String generated_output1 = ur.FirstName();
+		String generated_output2 = ur.LastName();
+		String generated_output3 = ur.Email();
+		String generated_output4 = ur.MobileNumber();
+		String generated_output5 = ur.Password();
 		boolean generated_output6 = ur.SampleEmailValidation();
 		
 		//Assert Statement
-		assertTrue(generated_output1);
-		assertTrue(generated_output2);
-		assertTrue(generated_output3);
-		assertTrue(generated_output4);
-		assertTrue(generated_output5);
+		assertEquals(Happy, generated_output1);
+		assertEquals(Happy, generated_output1);
+		assertEquals(Happy, generated_output2);
+		assertEquals(Happy, generated_output3);
+		assertEquals(Happy, generated_output4);
+		assertEquals(Happy, generated_output5);
 		assertTrue(generated_output6);
 }
 }
