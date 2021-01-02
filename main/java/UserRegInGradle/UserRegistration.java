4763a31e1457bb92f8182cf604dac737e14bd267
Komal-8080 <shindeg163@gmail.com>
2021-01-02 23:52:03 +0530

    Added: Method to validate and test FirstName

--- main/java/UserRegInGradle/UserRegistration.java
+++ main/java/UserRegInGradle/UserRegistration.java
@@ -1,8 +1,19 @@
 package UserRegInGradle;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	
+	public static boolean FirstName() {		
+		Pattern firstName = Pattern.compile("[A-Z]{1}[a-z]{2,}$");  
+		Matcher match = firstName.matcher("Komal");  
+		boolean patternValidity = match.matches();
+		System.out.println(patternValidity);
+		return patternValidity;		
+	}
+	
 	public static void main(String args[]) {
-		
+		FirstName();
 	}
 }
--- test/java/UserRegInGradle/UserRegistrationTest.java
+++ test/java/UserRegInGradle/UserRegistrationTest.java
@@ -1,5 +1,16 @@
 package UserRegInGradle;
 
-public class UserRegistrationTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class UserRegistrationTest {
+	
+	public static void FirstName() {
+		
+		UserRegistration ur = new UserRegistration();
+		boolean generated_output = ur.FirstName();
+		
+		//Assert Statement
+		assertTrue(generated_output);
+}
 }
